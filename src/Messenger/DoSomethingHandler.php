4c383a3732d83226e2e26489c0f89c62c932deba
Jan Mikeš <j.mikes@me.com>
2022-05-10 21:17:29 +0200

    Disable quorum queues + use CA cert for aws

--- src/Messenger/DoSomethingHandler.php
+++ src/Messenger/DoSomethingHandler.php
@@ -8,18 +8,22 @@ use App\Entity\PersistentMessage;
 use App\Messenger\Exception\PermanentFail;
 use App\Messenger\Exception\TemporaryFail;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 final class DoSomethingHandler implements MessageHandlerInterface
 {
     public function __construct(
         private EntityManagerInterface $entityManager,
+        private LoggerInterface $logger,
     ) {}
 
 
     public function __invoke(DoSomething $message): void
     {
-        $randomNumber = random_int(1, 10);
+        $this->logger->info('Consuming message seq: ' . $message->sequence);
+
+        $randomNumber = random_int(1, 30);
 
         if ($randomNumber === 1) {
             throw new TemporaryFail('This is temporary fail - will be retried in few seconds. Seq: ' . $message->sequence);
